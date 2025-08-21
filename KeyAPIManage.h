d2b0c52c4e45a60bf50242c705bed4d3917c66e1
phamhoangnhat <36319509+phamhoangnhat@users.noreply.github.com>
2025-08-22 06:37:30 +0700

    Update KeyAPIManage.h

--- KeyAPIManage.h
+++ KeyAPIManage.h
@@ -13,35 +13,6 @@ class KeyAPIManage : public QDialog {
 
 public:
 	QStringList listKeyAPIDefault = {
-        "AIzaSyB6RTT_W6RYT-mfFUL8uRQ_X6ueklCHzhI",
-        "AIzaSyBQHPVnQgzbw353scb6F6RSmFxrAWTTzCs",
-        "AIzaSyBWfOxz12WcSUK0yKN-3bgLPNjh6RtiJHg",
-        "AIzaSyCPS4ljZI46KqgRslaAn_-cbrajlQgT5wM",
-        "AIzaSyCYHvgagPnS2ArTneJTJrOhn6Tbt5vHiDY",
-
-        "AIzaSyCnlfVxs_ghQVOEwH3uo84zGiLHgYwCD2I",
-        "AIzaSyAUoeG00fbZkz-JhqWQBDtxQAw_8Uc8EAU",
-        "AIzaSyB_-E4PIfj-4NX56leSzztm7kurtvl0D_E",
-        "AIzaSyDp1S58addgafrACmzdcUjGNz0a4aJpjSM",
-        "AIzaSyArA4bbAbCckXsbI-KL84v2fqohEs_7c-U",
-
-        "AIzaSyAFe9dBEpmkzzz_bQRMODkXb4cExLoxYPA",
-        "AIzaSyARsZ8UJiVhVoEsWVznR8GCRHMQc_HYDtc",
-        "AIzaSyB_lhSjmT4xzwpaLRing1za0DooDwQwKbk",
-        "AIzaSyBJW4rGOK19iFT--5if-sSTCzJIyMgf33Y",
-        "AIzaSyBwcL631XCiaSNSBX3mqvjea0tGhHQSFok",
-
-        "AIzaSyCvlgtUnZmmEZj9QWwGL5pptsEjxRsHfOo",
-        "AIzaSyDHe0Oz35BAU8avEOevQKqcmX7EAu021Bg",
-        "AIzaSyBcjfMNlGw5hyLyyggQK6eqxziBNlBcuwE",
-        "AIzaSyAVzET6dBNiYRplNaGpOy-fOu2hDmNohZs",
-        "AIzaSyCym08bUu28sBC8UzKPUP8A3-OrjQsJ48E",
-
-        "AIzaSyB3o4a323WOsixUjguRi0ervNYietEv1d0",
-        "AIzaSyDFTLTqRtl7h4hxufVjgZQoe4yO3pXfDTY",
-        "AIzaSyAgC48ZICpf8bDcRoRQ2nSc1rW3zFvPzys",
-        "AIzaSyAviJR2YP7363kth8IhYIq2ExdYBJcP7dg",
-        "AIzaSyBm0TgHR3hEW9_lXhAUDUMIlsxucxEbOK8"
 	};
     QStringList listKeyAPI;
     QStringList listKeyAPIUser;
